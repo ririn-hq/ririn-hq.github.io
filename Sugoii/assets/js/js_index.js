b521f948cba6d6771c777e62a7e3ae862937034c
Tokisaki Nino <110331442+TokisakiNinoVn@users.noreply.github.com>
2023-07-19 00:26:59 +0700

    Update js_index.js

--- Sugoii/assets/js/js_index.js
+++ Sugoii/assets/js/js_index.js
@@ -12,11 +12,11 @@ var body = document.querySelector('body');
 body.classList.toggle('dark-mode');
 }
 
-// document.addEventListener("contextmenu", function(e) {
-//     e.preventDefault();
-// });
-// document.addEventListener("keydown", function(e) {
-//     if (e.ctrlKey && e.shiftKey && (e.keyCode === 73 || e.keyCode === 74)) {
-//         e.preventDefault();
-//     }
-// });
+document.addEventListener("contextmenu", function(e) {
+    e.preventDefault();
+});
+document.addEventListener("keydown", function(e) {
+    if (e.ctrlKey && e.shiftKey && (e.keyCode === 73 || e.keyCode === 74)) {
+        e.preventDefault();
+    }
+});
