7912805b4b6855fb44370514dc0813ce3351bb02
Nino <110331442+TokisakiNinoVn@users.noreply.github.com>
2023-07-31 20:42:44 +0700

    Update js_index.js

--- Sugoi_Last27_7/assets/js/js_index.js
+++ Sugoi_Last27_7/assets/js/js_index.js
@@ -27,11 +27,11 @@ document.body.addEventListener('click', function(event) {
 });
 
 
-// document.addEventListener("contextmenu", function(e) {
-//     e.preventDefault();
-// });
-// document.addEventListener("keydown", function(e) {
-//     if (e.ctrlKey && e.shiftKey && (e.keyCode === 73 || e.keyCode === 74)) {
-//         e.preventDefault();
-//     }
-// });
+document.addEventListener("contextmenu", function(e) {
+    e.preventDefault();
+});
+document.addEventListener("keydown", function(e) {
+    if (e.ctrlKey && e.shiftKey && (e.keyCode === 73 || e.keyCode === 74)) {
+        e.preventDefault();
+    }
+});
