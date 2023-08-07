a0b4ff1913a6d1c6f05bf38119e033709603617d
Nino <110331442+TokisakiNinoVn@users.noreply.github.com>
2023-08-07 23:23:50 +0700

    Update random.js

--- randomImg/random.js
+++ randomImg/random.js
@@ -1,20 +1,24 @@
+
 // Tạo một mảng để lưu trữ các link ảnh từ GitHub
 const imageUrls = [];
-
 // Thay đổi đường dẫn dưới đây thành đường dẫn thư mục ảnh của bạn.
 const githubRepoUrl = 'https://github.com/TokisakiNinoVn/TokisakiNinoVn/tree/main/ImgView';
+// Thay đổi đường dẫn dưới đây thành đường dẫn tới tệp tin proxy.php trên máy chủ của bạn
+const proxyUrl = 'https://nino.is-a.dev/proxy.php';
 
 // Hàm này lấy các link ảnh từ GitHub và hiển thị chúng lên trang web.
 async function displayRandomImages() {
     try {
         // Gọi API GitHub để lấy nội dung của thư mục công khai (thư mục ImgView)
-        const response = await fetch(githubRepoUrl);
+        const response = await fetch(proxyUrl + '?url=' + encodeURIComponent(githubRepoUrl));
         const data = await response.text();
 
         // Tạo một HTML tạm thời để phân tích nội dung
         const tempDiv = document.createElement('div');
         tempDiv.innerHTML = data;
 
+        // ...
+        // Các phần còn lại của hàm displayRandomImages() không thay đổi
         // Lấy tất cả các liên kết trong thư mục (đây có thể là các tệp hoặc thư mục)
         const links = tempDiv.querySelectorAll('a');
 
@@ -28,7 +32,6 @@ async function displayRandomImages() {
 
         // Hiển thị ngẫu nhiên một ảnh trong mảng imageUrls
         displayRandomImage();
-
     } catch (error) {
         console.error('Error fetching images:', error);
     }
@@ -52,3 +55,4 @@ function displayRandomImage() {
 
 // Gọi hàm để lấy các link ảnh từ GitHub và hiển thị chúng lên trang web
 displayRandomImages();
+
