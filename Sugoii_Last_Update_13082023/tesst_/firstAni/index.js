ba2103271da270abddc24df5490538eb6b61bf34
Nino <110331442+TokisakiNinoVn@users.noreply.github.com>
2023-08-28 21:11:07 +0700

    Delete Sugoii_Last_Update_13082023 directory

--- Sugoii_Last_Update_13082023/tesst_/firstAni/index.js
+++
@@ -1,7 +0,0 @@
-// script.js
-document.addEventListener("DOMContentLoaded", function() {
-  var openingEffect = document.querySelector(".opening-effect");
-  openingEffect.addEventListener("animationend", function() {
-    openingEffect.style.display = "none";
-  });
-});
