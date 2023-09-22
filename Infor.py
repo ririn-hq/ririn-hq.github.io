baa1b97048f6c5838da209d8362f2d476ac619c0
tokisakininovn <trieuduytancbg@gmail.com>
2023-09-22 18:04:25 +0700

    Update

---
+++ Infor.py
@@ -0,0 +1,9 @@
+MyInformayion = {
+    'name': 'Tokisaki Nino',
+    'age': 'N/a',
+    'gender': 'N/a',
+    'skill': ['Js', 'Python', 'C#', 'Java'],
+    'game': ['Genshin Impact', 'Honkai Impact 3rd'],
+    'like': ['Anime', 'Music', 'Game', 'Coding', 'Queen'],
+    'descript': "I am an Information Technology student. I'm a person who likes new things and I really like programming. I hope to become a good programmer in the future."
+}
--- assets/js/js.js
+++ assets/js/js.js
@@ -7,3 +7,14 @@
 //         e.preventDefault();
 //     }
 // });
+
+
+const MyInformayion = {
+    name: 'Tokisaki Nino',
+    age: 'N/a',
+    gender: 'N/a',
+    skill: ['Js', 'Python', 'C#', 'Java'],
+    game: ['Genshin Impact', 'Honkai Impact 3rd'],
+    like: ['Anime', 'Music', 'Game', 'Coding'],
+    descript: 'I am an Information Technology student. I\'m a person who likes new things and I really like programming. I hope to become a good programmer in the future.'
+}
