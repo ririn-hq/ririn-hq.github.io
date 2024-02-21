eb9068bd374bbd6aaf424a2f90326779d3bba3d6
tokisakininovn <trieuduytancbg@gmail.com>
2024-02-21 18:45:20 +0700

    wfsa

--- assets/js/js.js
+++ assets/js/js.js
@@ -21,76 +21,76 @@
 
 
 // Status Discord
-const statusDivLeft = document.getElementById("status");
-const tittleStatus = document.querySelector(".tittle_status");
+// const statusDivLeft = document.getElementById("status");
+// const tittleStatus = document.querySelector(".tittle_status");
 
-statusDivLeft.addEventListener("mouseenter", () => {
-    tittleStatus.classList.add("tittle_status-effect");
-});
+// statusDivLeft.addEventListener("mouseenter", () => {
+//     tittleStatus.classList.add("tittle_status-effect");
+// });
 
-statusDivLeft.addEventListener("mouseleave", () => {
-    tittleStatus.classList.remove("tittle_status-effect");
-});
-
-
+// statusDivLeft.addEventListener("mouseleave", () => {
+//     tittleStatus.classList.remove("tittle_status-effect");
+// });
 
 
-const userID = "906037896295878706"; //put your Discord user id here
-const statusDiv = document.getElementById("status"); // Chọn thẻ div có ID là "status"
-const avatarDiv = document.getElementById("my_avt");
-const statusText = document.querySelector(".tittle_status");
 
-async function fetchDiscordStatus() {
-    try {
-        const response = await axios.get(
-            `https://api.lanyard.rest/v1/users/${userID}`
-        );
+
+// const userID = "906037896295878706"; //put your Discord user id here
+// const statusDiv = document.getElementById("status"); // Chọn thẻ div có ID là "status"
+// const avatarDiv = document.getElementById("my_avt");
+// const statusText = document.querySelector(".tittle_status");
+
+// async function fetchDiscordStatus() {
+//     try {
+//         const response = await axios.get(
+//             `https://api.lanyard.rest/v1/users/${userID}`
+//         );
         
-        const { data } = response.data;
-        const { discord_status, activities } = data;
+//         const { data } = response.data;
+//         const { discord_status, activities } = data;
 
-        let backgroundColor;
-        switch (discord_status) {
-            case "online":
-                statusText.textContent = "Online";
-                backgroundColor = "#00ff1e";
-                filter = "unset";
-                avatarDiv.style.filter = filter;
-                break;
-            case "idle":
-                statusText.textContent = "Idle";
-                backgroundColor = "#f5c904";
-                filter = "unset";
-                avatarDiv.style.filter = "unset";
-                break;
-            case "dnd":
-                statusText.textContent = "Do not bother";
-                backgroundColor = "#f50408";
-                filter = "unset";
-                avatarDiv.style.filter = "unset";
-                break;
-            case "offline":
-                statusText.textContent = "Offline";
-                // backgroundColor = "#474646";
-                backgroundColor = "rgb(255, 0, 242);";
-                filter = "saturate(30%)";
-                avatarDiv.style.filter = filter;
-                break;
-            default:
-                statusText.textContent = "Unknown";
-                backgroundColor = "#cb2073";
-                break;
-        }
+//         let backgroundColor;
+//         switch (discord_status) {
+//             case "online":
+//                 statusText.textContent = "Online";
+//                 backgroundColor = "#00ff1e";
+//                 filter = "unset";
+//                 avatarDiv.style.filter = filter;
+//                 break;
+//             case "idle":
+//                 statusText.textContent = "Idle";
+//                 backgroundColor = "#f5c904";
+//                 filter = "unset";
+//                 avatarDiv.style.filter = "unset";
+//                 break;
+//             case "dnd":
+//                 statusText.textContent = "Do not bother";
+//                 backgroundColor = "#f50408";
+//                 filter = "unset";
+//                 avatarDiv.style.filter = "unset";
+//                 break;
+//             case "offline":
+//                 statusText.textContent = "Offline";
+//                 // backgroundColor = "#474646";
+//                 backgroundColor = "rgb(255, 0, 242);";
+//                 filter = "saturate(30%)";
+//                 avatarDiv.style.filter = filter;
+//                 break;
+//             default:
+//                 statusText.textContent = "Unknown";
+//                 backgroundColor = "#cb2073";
+//                 break;
+//         }
 
-        statusDiv.style.backgroundColor = backgroundColor;
-    } catch (error) {
-		console.error("Unable to get Discord status:", error);
-	}
-}
+//         statusDiv.style.backgroundColor = backgroundColor;
+//     } catch (error) {
+// 		console.error("Unable to get Discord status:", error);
+// 	}
+// }
 
-// Gọi hàm để lấy và cập nhật trạng thái Discord
-fetchDiscordStatus();
-setInterval(fetchDiscordStatus, 1000);
+// // Gọi hàm để lấy và cập nhật trạng thái Discord
+// fetchDiscordStatus();
+// setInterval(fetchDiscordStatus, 1000);
 
 
 
@@ -110,6 +110,6 @@ fetch(apiUrl)
         avatarElement.src = avatarUrl;
     })
     .catch(error => {
-    console.error('>> Error: ', error);
+        console.error('>> Error: ', error);
     });
 setInterval(fetch, 5000);
